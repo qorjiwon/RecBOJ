516b0b40d67c0df330a3926b7073a40b1447431f
Jiwon Baek <wldnjsl2001@naver.com>
2023-11-20 03:27:50 +0900

    Update RelatedProblem.tsx

--- ChromeExtension/extension_with_react/src/contentScript/RelatedProblem.tsx
+++ ChromeExtension/extension_with_react/src/contentScript/RelatedProblem.tsx
@@ -73,43 +73,47 @@ function RelatedProblem() {
     }
     return (
         <div key={rotate} className='ContainerToRender'>
-          <button className='reloading' onClick={() => handleRotate()}></button>
-        <div className='problem_container'>
-          <b className='problem_index'>연관 문제 1:</b>
-          <a
-            data-tip={`유사도: ${problems.problem0_similarity}%, 티어: ${problems.problem0_tier}, 분류: ${problems.problem0_tags}`}
-            href={urls.problem0}
-            className= 'RecommenedProblem'
-          >
-            {problems.problem0_titleKo}
-          </a>
-        </div>
-          <div className='problem_container'>
-          <b className='problem_index'>연관 문제 2:</b>
-          <a
-            data-tip={` 유사도: ${problems.problem1_similarity}%, 티어: ${problems.problem1_tier}, 분류: ${problems.problem1_tags}`}
-            href={urls.problem1}
-            className= 'RecommenedProblem'
-          >
-            {problems.problem1_titleKo}
-          </a>
-        </div>
-          <div className='problem_container'>
-          <b className='problem_index' >연관 문제 3:</b>
-          <a
-            data-tip={` 유사도: ${problems.problem2_similarity}%, 티어: ${problems.problem2_tier}, 분류: ${problems.problem2_tags}`}
-            href={urls.problem2}
-            className= 'RecommenedProblem'
-            style={{
-              marginRight: '10px',
-              }}
-          >
-            {problems.problem2_titleKo}
-          </a>
-        </div>
-        <b className='message'>{problems.message}</b>
-        <ReactTooltip place="top" type="dark" effect="solid"/>
-      </div>      
+            <div style={{ width: '96%', display: 'flex'}}>
+                <div className='problem_container'>
+                    <b className='problem_index'>연관 문제 1: </b>
+                    <a
+                      data-tip={`유사도: ${problems.problem0_similarity}%, 티어: ${problems.problem0_tier}, 분류: ${problems.problem0_tags}`}
+                      href={urls.problem0}
+                      className= 'RecommenedProblem'
+                    >
+                      {problems.problem0_titleKo}
+                    </a>
+                </div>
+                <div className='problem_container'>
+                    <b className='problem_index'>연관 문제 2: </b>
+                    <a
+                      data-tip={` 유사도: ${problems.problem1_similarity}%, 티어: ${problems.problem1_tier}, 분류: ${problems.problem1_tags}`}
+                      href={urls.problem1}
+                      className= 'RecommenedProblem'
+                    >
+                      {problems.problem1_titleKo}
+                    </a>
+                </div>
+                <div className='problem_container'>
+                    <b className='problem_index' >연관 문제 3: </b>
+                    <a
+                      data-tip={` 유사도: ${problems.problem2_similarity}%, 티어: ${problems.problem2_tier}, 분류: ${problems.problem2_tags}`}
+                      href={urls.problem2}
+                      className= 'RecommenedProblem'
+                      style={{
+                        marginRight: '10px',
+                        }}
+                    >
+                      {problems.problem2_titleKo}
+                    </a>
+                </div>
+                <b className='message'>{problems.message}</b>
+            </div>
+            <ReactTooltip place="top" type="dark" effect="solid"/>
+            <div style={{ width: '4%', display: 'flex', justifyContent: 'right'}}>
+                <button className='reloading' onClick={() => handleRotate()}></button>
+            </div>
+        </div>      
       
     );
 }
