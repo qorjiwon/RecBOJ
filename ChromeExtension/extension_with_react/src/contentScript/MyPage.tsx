a80da9317f2f119a47b4a3240c305a82b9f83e83
Jiwon Baek <wldnjsl2001@naver.com>
2023-11-20 15:15:20 +0900

    Update detail design

--- ChromeExtension/extension_with_react/src/contentScript/MyPage.tsx
+++ ChromeExtension/extension_with_react/src/contentScript/MyPage.tsx
@@ -167,7 +167,7 @@ function MyPage() {
                                                     data-tip={`${problem}번 풀러 가기`}
                                                     onClick={() => contentClick(`https://www.acmicpc.net/problem/${problem}`)}
                                                      style = {{display: 'flex', flexDirection: 'column'}}>
-                                                        <p style={{fontSize: '17px'}}><b>{problems.weak_tag_problems[selectedField_weak].explainations[index][1]}</b></p>
+                                                        <p style={{fontSize: '17px', padding: '8px'}}><b>{problems.weak_tag_problems[selectedField_weak].explainations[index][1]}</b></p>
                                                         <p style={{fontSize: '3px'}}><b><br></br></b></p>
                                                         <p >난이도: {problems.weak_tag_problems[selectedField_weak].explainations[index][2]}</p>
                                                         <p>평균 시도 횟수: {problems.weak_tag_problems[selectedField_weak].explainations[index][3]}</p>
@@ -179,7 +179,7 @@ function MyPage() {
                                 )}
                                 <div style={{ textAlign: 'right', paddingRight: '3%' }}>
                                     <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"></path>
-                                    <p data-tip = {`취약 유형은 ${problems.user_id}님의 해당 유형의 정답률, 푼 문제 수 등을 고려하여 산출돼요`}
+                                    <p data-tip = {`취약 유형은 ${problems.user_id}님의 해당 유형의 정답률, 푼 문제 수 등을 고려하여 산출돼요.`}
                                         className = 'qmark'>
                                         취약 유형이란?</p>
                                     <ReactTooltip place="left" type="success" effect="solid"/>
